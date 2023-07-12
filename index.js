fb3255b709110c41d2cc70f8a7275a7578eea9df
rafia02 <rafiabinterashed01@gmail.com>
2023-07-13 01:26:28 +0600

    searh api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express()
 const port = 5000
 
 app.use(cors())
+app.use(express.json())
 
 
 
@@ -26,44 +27,96 @@ async function run() {
 
         const catagoriesCollection = client.db("SRbnbHotel").collection("catagories")
         const hotelsCollection = client.db("SRbnbHotel").collection("hotels")
+        const reserveCollection = client.db("SRbnbHotel").collection("reserves")
+        const bookingCollection = client.db('SRbnbHotel').collection('bookings')
+
+
+        // app.get("/search", async(req, res)=>{
+        //     const {country, guest, rate}= req.query
+       
+        
+
+        //     const filter = {
+        //         "pleace.country" : country,
+        //         "room.guest": rate
+
+        //     }
+
+        //     const result = await hotelsCollection.find(filter).toArray()
+        //     res.send(result)
+            
+        // })
+
+
+
+          app.post('/booking', async(req, res) =>{
+            const book = req.body     
+            const result = await bookingCollection.insertOne(book)  
+            res.send(result)
+        })
+
+     
 
 
 
 
 
+        app.get('/reserve', async(req, res)=>{
+            const email = req.query.email 
+    
+            const filter = {email: email}
+            const result = await reserveCollection.find(filter).toArray()
+            res.send(result)
+        })
 
 
-        app.get("/catagories", async(req, res)=>{
+
+        app.post('/reserve', async(req, res)=>{
+            const info = req.body 
+            const result = await reserveCollection.insertOne(info)
+            res.send(result)
+        })
+
+
+
+        app.get("/catagories", async (req, res) => {
             const result = await catagoriesCollection.find({}).toArray()
             res.send(result)
         })
 
 
 
-        app.get("/hotels/:id", async(req, res)=>{
-            const  id = req.params.id
-            const filter = {catagory: id} 
+        app.get("/hotels/:id", async (req, res) => {
+            const id = req.params.id
+            const filter = { catagory: id }
             const result = await hotelsCollection.find(filter).toArray()
             res.send(result)
         })
 
 
-        app.get("/hotel/:id", async(req, res)=>{
+        app.get("/hotel/:id", async (req, res) => {
             const id = req.params.id
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const result = await hotelsCollection.findOne(filter)
             res.send(result)
         })
 
 
-        app.get("/hotels", async(req, res)=>{
+        app.get("/hotels", async (req, res) => {
             const result = await hotelsCollection.find({}).toArray()
             res.send(result)
         })
 
+
+
+
+        
+
+        
+
     } finally {
 
-   
+
     }
 }
 run().catch(console.dir);
